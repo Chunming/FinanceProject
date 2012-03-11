08af5d7b529642f14a7833520e00a0929a2ea9b9
Chun Ming <chunmingchin@gmail.com>
2012-03-10 20:13:00 -0800

    Obtained AAPLdata

--- hist.m
+++ hist.m
@@ -3,71 +3,106 @@ close all
 clc;
 
 
-fid1 = fopen('IBMbidHist.csv');
+fid1 = fopen('AAPLbidHist.csv');
 %bidSym = textscan(fid1, 'Ticker is %s');
 bid = textscan(fid1, 'Time: %d, Price: %.2f, Count: %d,');
 
-fid2 = fopen('IBMofrHist.csv');
+fid2 = fopen('AAPLofrHist.csv');
 %ofrSym = textscan(fid2, 'Ticker is %s');
 ofr = textscan(fid2, 'Time: %d, Price: %.2f, Count: %d,');
 
+fid3 = fopen('AAPLtotalHist.csv');
+%ofrSym = textscan(fid2, 'Ticker is %s');
+total = textscan(fid3, 'Time: %d, Price: %.2f, Count: %d,');
 
-bidTimeCurr = bid{1}();
-bidTimePrev = bid{1}();
+fid4 = fopen('AAPLdata.csv','w');
+
 
 i = 1;
 iStart = 1;
-timeStep = 1;
-iStart = 1;
-bidData = zeros(size(bid{1}), 4); % Store 4 elements per time step
-ofrData = zeros(size(ofr{1}), 4); %
-n = size(bid{1},1);
-
-while i < n
+iTimeStep = 1;
+totalData = zeros(size(total{1},1), 5); % Store 4 elements per time step
+nTotal = size(total{1},1); % Bid dim
+while i < nTotal
     
-    while bid{1}(iStart)==bid{1}(i)
+    while total{1}(iStart)==total{1}(i)
        i = i + 1;
-       if i > n
+       if i > nTotal
            break;
        end
     end
     iEnd = i - 1; % 6
     
     % Bid Data
-    bidPriceVect = bid{2}(iStart:iEnd);
-    bidFreqVect = bid{3}(iStart:iEnd);
-    bidVect = rude(double(bidFreqVect), bidPriceVect); % Run length coding
+    totalPriceVect = total{2}(iStart:iEnd);
+    totalFreqVect = total{3}(iStart:iEnd);
+    totalVect = rude(double(totalFreqVect), totalPriceVect); % Run length coding
     
-    m_bid = mean(bidVect);
-    v_bid = var(bidVect);
-    k_bid = kurtosis(bidVect); % Centered
-    y_bid = skewness(bidVect);
+    totalTime = total{1}(iStart);
+    m_total = mean(totalVect);
+    v_total = var(totalVect);
+    y_total = skewness(totalVect);
+    k_total = kurtosis(totalVect); % Centered
 
-    bidData(timeStep,1) = m_bid;
-    bidData(timeStep,2) = v_bid;
-    bidData(timeStep,3) = k_bid;
-    bidData(timeStep,4) = y_bid;
 
+    totalData(iTimeStep,1) = totalTime;
+    totalData(iTimeStep,2) = m_total;
+    totalData(iTimeStep,3) = v_total;
+    totalData(iTimeStep,4) = y_total;
+    totalData(iTimeStep,5) = k_total;
     
-    % Ofr Data
-    ofrPriceVect = ofr{2}(iStart:iEnd);
-    ofrFreqVect = ofr{3}(iStart:iEnd);
-    ofrVect = rude(double(ofrFreqVect), ofrPriceVect); % Run length coding
-    
-    m_ofr = mean(ofrVect);
-    v_ofr = var(ofrVect);
-    k_ofr = kurtosis(ofrVect); % Centered
-    y_ofr = skewness(ofrVect);
-
-    ofrData(timeStep,1) = m_ofr;
-    ofrData(timeStep,2) = v_ofr;
-    ofrData(timeStep,3) = k_ofr;
-    ofrData(timeStep,4) = y_ofr;
+    fprintf(fid4,'TimeStep: %d, m: %.2f, v: %.2f, y: %.2f, k: %.2f \n',...
+        totalTime, m_total, v_total, y_total, k_total);
     
     iStart = iEnd + 1;
-    timeStep = timeStep + 1;
+    iTimeStep = iTimeStep + 1;
     
 end 
+
+
+
+% j = 1;
+% jStart = 1;
+% jTimeStep = 1;
+% ofrData = zeros(size(ofr{1}), 5); %
+% nOfr = size(ofr{1},1); % Ofr dim
+% while j < nOfr
+%     
+%     while ofr{1}(jStart)==ofr{1}(j)
+%        j = j + 1;
+%        if j > nOfr
+%            break;
+%        end
+%     end
+%     jEnd = j - 1; % 6
+% 
+%     % Ofr Data
+%     ofrPriceVect = ofr{2}(jStart:jEnd);
+%     ofrFreqVect = ofr{3}(jStart:jEnd);
+%     ofrVect = rude(double(ofrFreqVect), ofrPriceVect); % Run length coding
+%     
+%     ofrTime = ofr{1}(jStart);
+%     m_ofr = mean(ofrVect);
+%     v_ofr = var(ofrVect);
+%     k_ofr = kurtosis(ofrVect); % Centered
+%     y_ofr = skewness(ofrVect);
+% 
+%     ofrData(jTimeStep,1) = ofrTime;
+%     ofrData(jTimeStep,2) = m_ofr;
+%     ofrData(jTimeStep,3) = v_ofr;
+%     ofrData(jTimeStep,4) = k_ofr;
+%     ofrData(jTimeStep,5) = y_ofr;    
+%     
+%     jStart = jEnd + 1;
+%     jTimeStep = jTimeStep + 1;
+%     
+% end 
+
+% for i = 1:size(ofr{1},1)
+%    if(bidData(i,1) ~= ofrData(i,1))
+%       fprintf('TimeStep at bid/ofrData at %d not match \n', i);
+%    end
+% end
 
 
 % bidIdx : Index of higest freq element
@@ -90,3 +125,5 @@ end
 
 fclose(fid1);
 fclose(fid2);
+fclose(fid3);
+fclose(fid4);
